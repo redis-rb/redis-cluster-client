3e83296471802b12cb52c46270a300a2414b58c4
Taishi Kasuga <supercaracal@yahoo.com>
2022-06-28 15:08:27 +0900

    fix

--- test/test_against_cluster_scale.rb
+++ test/test_against_cluster_scale.rb
@@ -9,38 +9,28 @@ class TestAgainstClusterScale < TestingWrapper
     :alpha
   end
 
-  def setup
-    config = ::RedisClient::ClusterConfig.new(
-      nodes: TEST_NODE_URIS,
-      replica: true,
-      fixed_hostname: TEST_FIXED_HOSTNAME,
-      **TEST_GENERIC_OPTIONS
-    )
-    @client = ::RedisClient::Cluster.new(config)
-    @controller = ClusterController.new(
-      TEST_NODE_URIS,
-      replica_size: TEST_REPLICA_SIZE,
-      **TEST_GENERIC_OPTIONS.merge(timeout: 30.0)
-    )
-  end
-
   def teardown
-    @client.close
-    @controller.close
+    @client&.close
+    @controller&.close
   end
 
   def test_01_scale_out
+    @client = build_testee_client(TEST_NODE_URIS)
+    @controller = build_cluster_controller(TEST_NODE_URIS)
+
     @client.pipelined { |pi| NUMBER_OF_KEYS.times { |i| pi.call('SET', "key#{i}", i) } }
     wait_for_replication
 
-    primary_url = "#{TEST_REDIS_SCHEME}://#{TEST_REDIS_HOST}:#{TEST_REDIS_PORTS.max + 1}"
-    replica_url = "#{TEST_REDIS_SCHEME}://#{TEST_REDIS_HOST}:#{TEST_REDIS_PORTS.max + 2}"
+    primary_url, replica_url = build_additional_node_urls
     @controller.scale_out(primary_url: primary_url, replica_url: replica_url)
 
     NUMBER_OF_KEYS.times { |i| assert_equal(i.to_s, @client.call('GET', "key#{i}"), "Case: key#{i}") }
   end
 
   def test_02_scale_in
+    @client = build_testee_client(TEST_NODE_URIS + build_additional_node_urls)
+    @controller = build_cluster_controller(TEST_NODE_URIS + build_additional_node_urls)
+
     @controller.scale_in
     NUMBER_OF_KEYS.times { |i| assert_equal(i.to_s, @client.call('GET', "key#{i}"), "Case: key#{i}") }
   end
@@ -51,5 +41,29 @@ class TestAgainstClusterScale < TestingWrapper
     client_side_timeout = TEST_TIMEOUT_SEC + 1.0
     server_side_timeout = (TEST_TIMEOUT_SEC * 1000).to_i
     @client.blocking_call(client_side_timeout, 'WAIT', TEST_REPLICA_SIZE, server_side_timeout)
+  end
+
+  def build_testee_client(nodes)
+    config = ::RedisClient::ClusterConfig.new(
+      nodes: nodes,
+      replica: true,
+      fixed_hostname: TEST_FIXED_HOSTNAME,
+      **TEST_GENERIC_OPTIONS
+    )
+    ::RedisClient::Cluster.new(config)
+  end
+
+  def build_cluster_controller(nodes)
+    ClusterController.new(
+      nodes,
+      replica_size: TEST_REPLICA_SIZE,
+      **TEST_GENERIC_OPTIONS.merge(timeout: 30.0)
+    )
+  end
+
+  def build_additional_node_urls
+    primary_url = "#{TEST_REDIS_SCHEME}://#{TEST_REDIS_HOST}:#{TEST_REDIS_PORTS.max + 1}"
+    replica_url = "#{TEST_REDIS_SCHEME}://#{TEST_REDIS_HOST}:#{TEST_REDIS_PORTS.max + 2}"
+    [primary_url, replica_url]
   end
 end
