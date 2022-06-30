7a9c31487a83f5ca30ae6477ba173e18dae0db37
Taishi Kasuga <proxy0721@gmail.com>
2022-06-30 18:31:58 +0900

    test: add benchmark test (#76)

---
+++ test/benchmark_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# @see https://ruby-doc.org/stdlib-3.0.1/libdoc/minitest/rdoc/Minitest/Benchmark.html
+
+require 'minitest/benchmark'
+require 'minitest/autorun'
+require 'redis_client'
+require 'redis_cluster_client'
+require 'testing_constants'
+
+case ENV.fetch('REDIS_CONNECTION_DRIVER', 'ruby')
+when 'hiredis' then require 'hiredis-client'
+end
+
+class BenchmarkWrapper < Minitest::Benchmark; end
