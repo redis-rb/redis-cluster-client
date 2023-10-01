d7b508a20e412ad7401f8208b7cf0ac19e591f01
Taishi Kasuga <proxy0721@gmail.com>
2023-10-01 15:18:06 +0900

    perf: add an environment variable to be able to configure a data type of slots and nodes mapping (#283)

---
+++ test/ips_slot_node_mapping.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'benchmark/ips'
+require 'redis_cluster_client'
+
+module IpsSlotNodeMapping
+  ELEMENTS = %w[foo bar baz].freeze
+  SIZE = ::RedisClient::Cluster::Node::SLOT_SIZE
+
+  module_function
+
+  def run
+    ca = ::RedisClient::Cluster::Node::CharArray.new(SIZE, ELEMENTS)
+    arr = Array.new(SIZE)
+
+    print_letter('Mappings between slots and nodes')
+    fullfill(ca)
+    fullfill(arr)
+    bench({ ca.class.name.split('::').last => ca, arr.class.name => arr })
+  end
+
+  def print_letter(title)
+    print "################################################################################\n"
+    print "# #{title}\n"
+    print "################################################################################\n"
+    print "\n"
+  end
+
+  def fullfill(arr)
+    SIZE.times { |i| arr[i] = ELEMENTS[i % ELEMENTS.size] }
+  end
+
+  def bench(subjects)
+    Benchmark.ips do |x|
+      x.time = 5
+      x.warmup = 1
+
+      subjects.each do |subtitle, arr|
+        x.report(subtitle) do
+          arr[0]
+        end
+      end
+
+      x.compare!
+    end
+  end
+end
+
+IpsSlotNodeMapping.run
