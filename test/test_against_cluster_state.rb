c05234a5594b734df5f2d005a8ffcf56790f36f0
Taishi Kasuga <proxy0721@gmail.com>
2024-02-17 17:37:45 +0900

    test: ensure the transaction is legit durling the resharding (#319)

--- test/test_against_cluster_state.rb
+++ test/test_against_cluster_state.rb
@@ -62,11 +62,14 @@ class TestAgainstClusterState < TestingWrapper
     def test_the_state_of_cluster_resharding_with_transaction
       do_resharding_test do |keys|
         @client.multi do |tx|
-          keys.each { |key| tx.call('SET', key, key) }
+          keys.each do |key|
+            tx.call('SET', key, '0')
+            tx.call('INCR', key)
+          end
         end
 
         keys.each do |key|
-          want = key
+          want = '1'
           got = @client.call('GET', key)
           assert_equal(want, got, "Case: GET: #{key}")
         end
