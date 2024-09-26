4a26cdf69d2358c40d6d96169245ca68aff1297c
Taishi Kasuga <kasuga@livesense.co.jp>
2024-09-26 10:08:55 +0900

    fix

--- test/test_against_cluster_scale.rb
+++ test/test_against_cluster_scale.rb
@@ -187,9 +187,9 @@ module TestAgainstClusterScale
       end
 
       def do_test_after_scaled_out
-        NUMBER_OF_KEYS.times.group_by { |i| i / HASH_TAG_GRAIN }.each_value do |numbers|
-          keys = numbers.map { |i| "{group#{i / HASH_TAG_GRAIN}}:key#{i}" }
-          got = @client.multi(watch: (i / HASH_TAG_GRAIN).odd? ? nil : keys) do |tx|
+        NUMBER_OF_KEYS.times.group_by { |i| i / HASH_TAG_GRAIN }.each do |group, numbers|
+          keys = numbers.map { |i| "{group#{group}}:key#{i}" }
+          got = @client.multi(watch: group.odd? ? nil : keys) do |tx|
             keys.each { |key| tx.call('INCR', key) }
           end
 
@@ -199,10 +199,10 @@ module TestAgainstClusterScale
       end
 
       def do_test_after_scaled_in
-        NUMBER_OF_KEYS.times.group_by { |i| i / HASH_TAG_GRAIN }.each_value do |numbers|
-          keys = numbers.map { |i| "{group#{i / HASH_TAG_GRAIN}}:key#{i}" }
+        NUMBER_OF_KEYS.times.group_by { |i| i / HASH_TAG_GRAIN }.each do |group, numbers|
+          keys = numbers.map { |i| "{group#{group}}:key#{i}" }
           got = retryable(attempts: MAX_ATTEMPTS) do
-            @client.multi(watch: (i / HASH_TAG_GRAIN).odd? ? nil : keys) do |tx|
+            @client.multi(watch: group.odd? ? nil : keys) do |tx|
               keys.each { |key| tx.call('INCR', key) }
             end
           end
