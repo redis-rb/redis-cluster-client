f0d231e4926b2dccbe7cf0f7f0f6da036b6c653b
Brian Bernstein <brian.bernstein@braze.com>
2026-05-15 11:36:08 -0400

    Make exec metadata a set of indices.

--- lib/redis_client/cluster/pipeline.rb
+++ lib/redis_client/cluster/pipeline.rb
@@ -127,7 +127,7 @@ class RedisClient
         @seed = seed
         @pipelines = nil
         @size = 0
-        @multi_exec_metadata = {}
+        @multi_exec_indices = Set.new
       end
 
       def call(*args, **kwargs, &block)
@@ -172,10 +172,7 @@ class RedisClient
         slots = transaction.pipeline._commands.map { |command| @router.find_slot(command) }.compact.uniq
         raise ::RedisClient::Cluster::Transaction::ConsistencyError.new('unable to determine slot').with_config(@router.config) if transaction.node_key.nil? || slots.size != 1
 
-        @multi_exec_metadata[@size] = {
-          outer: @size,
-          node_key: transaction.node_key
-        }
+        @multi_exec_indices.add(@size)
 
         transaction.pipeline._commands.zip(transaction.pipeline._blocks || []).each do |command, block|
           append_pipeline_noreply(transaction.node_key).call_once_v(command, &block)
@@ -218,7 +215,7 @@ class RedisClient
           else
             all_replies ||= Array.new(@size)
             @pipelines[node_key].outer_indices.each_with_index do |outer, inner|
-              if @multi_exec_metadata.key?(outer) && v[inner].is_a?(Array)
+              if @multi_exec_indices.include?(outer) && v[inner].is_a?(Array)
                 v[inner].each do |result|
                   if result.is_a?(::RedisClient::CommandError)
                     errors ||= {}
